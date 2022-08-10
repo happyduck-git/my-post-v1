0991034d8781ebab1eac23144866890d40a2372b
GG <rkrudtls@gmail.com>
2022-08-10 23:15:11 +0900

    no change

--- my-post-v1/src/main/java/com/ggshin/mypostv1/index/IndexController.java
+++ my-post-v1/src/main/java/com/ggshin/mypostv1/index/IndexController.java
@@ -5,13 +5,10 @@ import lombok.NoArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 import javax.validation.Valid;
 
