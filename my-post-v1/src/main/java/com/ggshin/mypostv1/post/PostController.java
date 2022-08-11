ec3369a54f5003ded14c9a51bfb1b6f20849e86b
GGShin <ggbetterus@gmail.com>
2022-08-11 20:15:06 +0900

    Use UserDetails to find member for PostController

--- my-post-v1/src/main/java/com/ggshin/mypostv1/post/PostController.java
+++ my-post-v1/src/main/java/com/ggshin/mypostv1/post/PostController.java
@@ -2,17 +2,25 @@ package com.ggshin.mypostv1.post;
 
 import com.ggshin.mypostv1.member.Member;
 import com.ggshin.mypostv1.member.MemberService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.WebAuthenticationDetails;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 import javax.validation.constraints.Positive;
+import java.util.Collection;
 import java.util.List;
 
+@Slf4j
 @Controller
 @Validated
 @RequestMapping("/v1/post")
@@ -35,7 +43,11 @@ public class PostController {
     }
 
     @PostMapping
-    public ResponseEntity postPost(@Valid PostDto.Post postDto) {
+    public ResponseEntity postPost(AuthenticationManager authenticationManager, @Valid PostDto.Post postDto) {
+
+        String sessionId = ((WebAuthenticationDetails)SecurityContextHolder.getContext().getAuthentication().getDetails()).getSessionId();
+        log.info("PostController SessionId: {}", sessionId);
+        //UserDetail 활용해서 member 찾는 거 시도해보기!
 
         //postDto to Post so that Service can do its job
         Post post = postMapper.postDtoToPost(postDto);
