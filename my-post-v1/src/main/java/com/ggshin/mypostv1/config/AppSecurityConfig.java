5eb5bc0df35e5d9033913e8c63f4fe9c56613b1c
GG <rkrudtls@gmail.com>
2022-08-15 23:44:05 +0900

    Comment out unnecessary codes

--- my-post-v1/src/main/java/com/ggshin/mypostv1/config/AppSecurityConfig.java
+++ my-post-v1/src/main/java/com/ggshin/mypostv1/config/AppSecurityConfig.java
@@ -4,13 +4,12 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.core.context.SecurityContext;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.context.HttpSessionSecurityContextRepository;
-import org.springframework.security.web.context.RequestAttributeSecurityContextRepository;
+
 
 @Configuration
 public class AppSecurityConfig {
@@ -20,23 +19,22 @@ public class AppSecurityConfig {
         return PasswordEncoderFactories.createDelegatingPasswordEncoder();
     };
 
-
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http.csrf().disable();
         http.headers().frameOptions().disable();
-        http.securityContext((securityContext) -> securityContext.securityContextRepository(new HttpSessionSecurityContextRepository()))
-                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.ALWAYS));
+
+        /* securityContext & sessionManagement는 없어도 정상적으로 동작함! => 이 둘의 기능 더 자세히 알아보기 */
+//        http.securityContext((securityContext) -> securityContext.securityContextRepository(new HttpSessionSecurityContextRepository()))
+//                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.ALWAYS));
 
         http.authorizeRequests()
                 .antMatchers("/v1/post/**").authenticated()
                 .anyRequest().permitAll()
                 .and()
                 .formLogin()
-                .loginPage("/login")
-                .permitAll(); //새로 추가 (필요없으면 삭제)
+                .loginPage("/login");
 
         return http.build();
     }
-
 }
